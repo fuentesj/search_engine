38233729591a830f94089adfa3bb01148794b7d0
jonathan <jonathan.wilfredo.fuentes@gmail.com>
2015-10-15 07:27:21 -0700

    Fixed CORS issue with back-end server by adding Allow-Access-Control-Origin:* Header to HTTP response. Fixed type in README name Including local copy of AngularJS

--- server/api.py
+++ server/api.py
@@ -3,14 +3,18 @@ searchEngine = SearchEngine()
 from flask import Flask, request, url_for
 app = Flask(__name__)
 
-@app.route('/search_results', methods = ['POST'])
+@app.route('/search_results', methods = ['POST', 'GET'])
 def api_root():
 	return "test"
 	# Commented out for debugging purposes
 	# search_term = request.args.get('search_term')
 	# searchEngine.search(search_term)
 	# return 'POST'
-	
+
+@app.after_request
+def after_request(response):
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	return response
 
 if __name__ == '__main__':
 	app.run()
