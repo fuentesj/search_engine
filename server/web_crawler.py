575c65e90ec88be63a61d115c4b945a35862e9d6
jonathan <jonathan.wilfredo.fuentes@gmail.com>
2015-10-28 20:32:32 -0700

    Added search() function to web_crawler that now prints out all <a> tags for each path that is deemed searchable within the domain's robots.txt. Added exception handling for cases where there is no robots.txt or for other 4** error codes.

--- server/web_crawler.py
+++ server/web_crawler.py
@@ -1,23 +1,31 @@
 import re
 import urllib2
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
 class WebCrawler:
 
 	def __init__(self):
 		self.seed_urls = []
-		self.inclusion_dict = {}
+		self.allowed_paths_list = []
 		self.ALLOW_KEYWORD = "Allow:"
 
-	def search(self, seed_url):
-		self.__fetch_disallowed_paths(seed_url)
-		html_page = urllib2.urlopen(seed_url)
-		soup = BeautifulSoup(html_page)
-		for link in soup.findAll('a'):
-			print link.get('href')
+	def search(self, base_domain):
+		try:
+			self.fetch_allowed_paths(base_domain)
+			for path in self.allowed_paths_list:
+				try:
+				 	html = urllib2.urlopen(base_domain+path)
+				 	soup = BeautifulSoup(html)
+					if soup is not None:
+						print soup.find_all('a')
+				except urllib2.HTTPError, error:
+					print "An exception with an error code " + str(error.code) + " occurred while crawling " + base_domain + path
+		except urllib2.HTTPError:
+			print "No robots.txt was found! Exiting now."
+		
 
-	def fetch_allowed_paths(self, base_url):
-		http_response = urllib2.urlopen(base_url + "/robots.txt")
+	def fetch_allowed_paths(self, base_domain):
+		http_response = urllib2.urlopen(base_domain + "/robots.txt")
 		response_data = http_response.read()
 		robots_exclusion_list = response_data.split('\n');
 		for entry in robots_exclusion_list:
@@ -27,4 +35,4 @@ class WebCrawler:
 			 	if action == self.ALLOW_KEYWORD:
 					path_match = re.search('\/.+', entry)
 					path = path_match.group(0)
-					self.inclusion_dict[path] = action
+					self.allowed_paths_list.append(path)
